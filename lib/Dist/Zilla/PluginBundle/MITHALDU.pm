9e082284065c3d84596ed92566935dc79d5136ef
Christian Walde <walde.christian@googlemail.com>
2012-01-09 16:11:30 +0100

    fork to MITHALDU

--- lib/Dist/Zilla/PluginBundle/MITHALDU.pm
+++ lib/Dist/Zilla/PluginBundle/MITHALDU.pm
@@ -1,6 +1,6 @@
 use strict;
 use warnings;
-package Dist::Zilla::PluginBundle::DAGOLDEN;
+package Dist::Zilla::PluginBundle::MITHALDU;
 # VERSION
 
 # Dependencies
@@ -106,7 +106,7 @@ has weaver_config => (
   is      => 'ro',
   isa     => 'Str',
   lazy    => 1,
-  default => sub { $_[0]->payload->{weaver_config} || '@DAGOLDEN' },
+  default => sub { $_[0]->payload->{weaver_config} || '@MITHALDU' },
 );
 
 has git_remote => (
@@ -295,14 +295,14 @@ __PACKAGE__->meta->make_immutable;
 
 1;
 
-# ABSTRACT: Dist::Zilla configuration the way DAGOLDEN does it
+# ABSTRACT: Dist::Zilla configuration the way MITHALDU does it
 # COPYRIGHT
 
 __END__
 
 
 =for stopwords
-autoprereq dagolden fakerelease pluginbundle podweaver
+autoprereq mithaldu fakerelease pluginbundle podweaver
 taskweaver uploadtocpan dist ini
 
 =for Pod::Coverage configure mvp_multivalue_args old_meta
@@ -312,9 +312,12 @@ taskweaver uploadtocpan dist ini
 = SYNOPSIS
 
   # in dist.ini
-  [@DAGOLDEN]
+  [@MITHALDU]
 
 = DESCRIPTION
+
+This module is forked off of [Dist::Zilla::PluginBundle::DAGOLDEN] and modified
+to suit my own tastes. He did most of the work.
 
 This is a [Dist::Zilla] PluginBundle.  It is roughly equivalent to the
 following dist.ini:
@@ -341,7 +344,7 @@ following dist.ini:
   [OurPkgVersion]     ; add $VERSION = ... to all files
   [InsertCopyright    ; add copyright at "# COPYRIGHT"
   [PodWeaver]         ; generate Pod
-  config_plugin = @DAGOLDEN ; my own plugin allows Pod::WikiDoc
+  config_plugin = @MITHALDU ; my own plugin allows Pod::WikiDoc
 
   ; generated files
   [License]           ; boilerplate license
@@ -460,7 +463,7 @@ robust than just the version number when parsing versions
 * {major_version} -- overrides the major version set by AutoVersion
 * {fake_release} -- swaps FakeRelease for UploadToCPAN. Mostly useful for
 testing a dist.ini without risking a real release.
-* {weaver_config} -- specifies a Pod::Weaver bundle.  Defaults to @DAGOLDEN.
+* {weaver_config} -- specifies a Pod::Weaver bundle.  Defaults to @MITHALDU.
 * {stopwords} -- add stopword for Test::PodSpelling (can be repeated)
 * {no_critic} -- omit Test::Perl::Critic tests
 * {no_spellcheck} -- omit Test::PodSpelling tests
